f2bba3f8ff1dc3e1adac0f08b7d8daffcf77ce2a
cody-mckeon <codymckeon@gmail.com>
2025-05-07 13:38:24 -0700

    Update rental_analysis.py
    Updated for CapEx and IRR calculation

--- modules/rental_analysis.py
+++ modules/rental_analysis.py
@@ -1,11 +1,12 @@
 # NOI, CoC, Cap Rate Functions
-def calculate_noi(gross_rent, vacancy_rate, taxes, insurance, maintenance, mgmt_fee, capex):
+def calculate_noi(gross_rent, vacancy_rate, taxes, insurance, maintenance, mgmt_fee, capex_reserve):
     effective_rent = gross_rent * (1 - vacancy_rate)
-    total_expenses = taxes + insurance + maintenance + mgmt_fee + capex
+    total_expenses = taxes + insurance + maintenance + mgmt_fee + capex_reserve
     return effective_rent - total_expenses
 
-def calculate_cash_on_cash(annual_noi, annual_debt_service, down_payment):
-    return (annual_noi - annual_debt_service) / down_payment
+def calculate_cash_on_cash(annual_noi, annual_debt_service, down_payment, make_ready_cost, closing_costs):
+    total_investment = down_payment + make_ready_cost + closing_costs
+    return (annual_noi - annual_debt_service) / total_investment
 
 def calculate_cap_rate(annual_noi, purchase_price):
     return annual_noi / purchase_price
