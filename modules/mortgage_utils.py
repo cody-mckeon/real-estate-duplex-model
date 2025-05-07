20625cf2ebc98d4057cd9d952356e8b4e3da1faa
cody-mckeon <codymckeon@gmail.com>
2025-05-07 13:13:13 -0700

    Update mortgage_utils.py
    Mortgage Payments function

--- modules/mortgage_utils.py
+++ modules/mortgage_utils.py
@@ -1,1 +1,9 @@
 # Mortgage-related functions
+
+def mortgage_payment(principal, annual_rate, years):
+    r = annual_rate / 12
+    n = years * 12
+    if r == 0:
+        return principal / n
+    return principal * (r * (1 + r)**n) / ((1 + r)**n - 1)
+
